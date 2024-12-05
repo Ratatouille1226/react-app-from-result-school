c63acfe74e6b0ed42a952f4874992aa3cebcc19d
Andrey <kotlyar.andrey03@mail.ru>
2024-12-05 17:49:49 +0300

    Вернул базовую разметку

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import ReactLogo from './assets/react.svg?react';
+import ReactLogo from './assets/react.svg';
 
 export const App = () => {
+	const currentYear = `Текущий год: ${new Date().getFullYear()}`; //декларативный стиль
 	return (
-		<div>
-			<h1>Hello World!</h1>
-            <ReactLogo />
+		<div className="App">
+			<header className="App-header">
+				<img src={ReactLogo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{currentYear}</p>
+			</header>
 		</div>
 	);
 };
+
